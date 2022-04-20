302cbf22ab7e7d6c549cbdf4bd4cc152162fef28
Mikhail <mmk_kz@mail.ru>
2022-04-20 16:38:51 +0700

    lesson 51

--- Java. From simple to complex/OOP/src/com/javalesson/collections/collectionsClass/CollectionsRunner.java
+++ Java. From simple to complex/OOP/src/com/javalesson/collections/collectionsClass/CollectionsRunner.java
@@ -1,12 +1,39 @@
 package com.javalesson.collections.collectionsClass;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
 
-public class CollectionsRunner {
+public  class CollectionsRunner {
     public static void main(String[] args) {
+        List<Card> deckOfCards = new ArrayList<>();
+        for (Card.Face face : Card.Face.values()) {
+            for (Card.Suit suit:Card.Suit.values()) {
+                deckOfCards.add(new Card(suit, face));
+            }
+        }
+
+/*        System.out.println("Original deck of cards");
+        for(int i = 0; i < deckOfCards.size(); i++) {
+            System.out.printf("%-20s %s", deckOfCards.get(i), (i + 1) % 4 ==0 ? "\n" : " ");
+        }*/
+
+        Collections.shuffle(deckOfCards);
+
+        System.out.println("\n\nCards after shuffle");
+
+        for(int i = 0; i < deckOfCards.size(); i++) {
+            System.out.printf("%-20s %s", deckOfCards.get(i), (i + 1) % 4 ==0 ? "\n" : " ");
+        }
+
+        Collections.sort(deckOfCards,new CardComparator());
+
+        //Collections.sort(deckOfCards);
+        System.out.println("\n\nCards after sorting");
+        for(int i = 0; i < deckOfCards.size(); i++) {
+            System.out.printf("%-20s %s", deckOfCards.get(i), (i + 1) % 4 ==0 ? "\n" : " ");
+        }
+    }
+   /* public static void main(String[] args) {
         List<String> colors = new ArrayList<>();
 
         colors.add("yellow");
@@ -18,5 +45,68 @@ public class CollectionsRunner {
         System.out.println("List before sorting " + colors);
         Collections.sort(colors);
         System.out.println("List after sorting " + colors);
-    }
+    }*/
+    public static class Card implements Comparable<Card>{
+
+       private enum Suit {SPADES,HEARTS,CLUBS,DIMONDS}
+
+        private enum Face{Ace,Deuce,Three,Four,Five,Six,Eight,Nine,Ten,Jack,Queen,King}
+
+        private final Suit suit;
+
+        private final Face face;
+
+       public Card(Suit suit, Face face) {
+           this.suit = suit;
+           this.face = face;
+       }
+
+       public Suit getSuit() {
+           return suit;
+       }
+
+       public Face getFace() {
+           return face;
+       }
+
+       @Override
+       public String toString() {
+           return face + " of " + suit;
+       }
+
+       @Override
+       public int compareTo(Card card) {
+
+           Face[] values = Face.values();
+           List<Face> faces = Arrays.asList(values);
+
+           if(faces.indexOf(this.face) < faces.indexOf(card.getFace())) {
+               return -1;
+           } else if (faces.indexOf(this.face) > faces.indexOf(card.getFace())) {
+               return +1;
+           } else if (faces.indexOf(this.face) == faces.indexOf(card.getFace())) {
+               return String.valueOf(suit).compareTo(String.valueOf(card.getSuit()));
+           }
+           return 0;
+
+
+       }
+   }
+
+   public static class CardComparator implements Comparator<Card> {
+
+       @Override
+       public int compare(Card card1, Card card2) {
+
+           List<Card.Face> faces = Arrays.asList(Card.Face.values());
+           if(faces.indexOf(card1.getFace()) < faces.indexOf(card2.getFace())) {
+               return 1;
+           } else if (faces.indexOf(card1.getFace()) > faces.indexOf(card2.getFace())) {
+               return -1;
+           } else if (faces.indexOf(card1.getFace()) == faces.indexOf(card2.getFace())) {
+               return String.valueOf(card1.getSuit()).compareTo(String.valueOf(card2.getSuit()));
+           }
+           return 0;
+       }
+   }
 }
