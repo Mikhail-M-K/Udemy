8abc1ffcd690a5138d4173ff640826d6674a8c29
Mikhail <mmk_kz@mail.ru>
2022-05-05 17:35:02 +0700

    lesson 59

---
+++ Java. From simple to complex/OOP/src/com/javalesson/collections/set/SetRunner.java
@@ -0,0 +1,42 @@
+package com.javalesson.collections.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetRunner {
+    public static void main(String[] args) {
+        Set<Car> sixCars = new HashSet<>();
+        sixCars.add(new Car("VW","Golf", 45));
+        sixCars.add(new Car("Audi","A3", 60));
+        sixCars.add(new Car("VW","Polo", 25));
+        sixCars.add(new Car("BMW","Z4", 120));
+        sixCars.add(new Car("BMW","440i", 200));
+
+        Set<Car> europaCars = new HashSet<>();
+        europaCars.add(new Car("Toyota", "Auris", 40));
+        europaCars.add(new Car("Reno", "Clio", 30));
+        europaCars.add(new Car("Reno", "Megan", 50));
+        europaCars.add(new Car("VW", "Golf", 45));
+        europaCars.add(new Car("VW", "Polo", 35));
+
+        Set<Car> uniqueCars = new HashSet<>(sixCars);
+        uniqueCars.addAll(europaCars);  //объединение
+//        print(uniqueCars);
+
+        sixCars.retainAll(europaCars); //общие элементы
+//        europaCars.removeAll(sixCars); //вычитание
+        uniqueCars.removeAll(sixCars);
+
+        print(uniqueCars);
+    }
+
+    private static void print(Set<Car> cars) {
+        System.out.printf("%-20s %-20s %-20s\n", "Car brand","Model","Price per day");
+
+        for (Car car: cars) {
+            System.out.printf("%-20s %-20s %-20s\n", car.getCarBrand(),car.getModel(),car.getPricePerDay());
+        }
+    }
+
+
+}
